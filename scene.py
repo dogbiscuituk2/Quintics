d3ea286d434bf6bc85a6f1ae3a7d99c8e861ea17
John Kerr <john@jmkerr.com>
2024-10-03 14:57:58 +0100

    xfer

--- scene.py
+++ scene.py
@@ -11,38 +11,82 @@ class Equation(Scene):
         self.play(TransformFromCopy(t1[1], t2[1]))
         self.wait()
 
-class Quintic02(Scene):
+class Quintic01(Scene):
     def construct(self):
 
         def hide_chars(chars):
             for char in chars:
                 char.set_color(BLACK)
 
-        Y = MathTex(r'y^1\ ')
-        E1 = MathTex(r'=\ 1x^5', r'+\ ax^4', r'+\ bx^3', r'+\ cx^2', r'+\ dx^1', r'+\ ex^0')
-        E3 = MathTex(r'=\ 1z^5', r'+\ 0z^4', r'+\ pz^3', r'+\ qz^2', r'+\ rz^1', r'+\ sz^0')
+        pause = 1;
+
+        Y1 = MathTex(r'(ay^1\ ')
+        E1 = MathTex(r'=\ 1x^5)', r'+\ ax^4)', r'+\ bx^3)', r'+\ cx^2)', r'+\ dx^1)', r'+\ ex^0)')
+        E3 = MathTex(r'=\ 1z^5)', r'+\ 0z^4)', r'+\ pz^3)', r'+\ qz^2)', r'+\ rz^1)', r'+\ sz^0)')
+        Y2 = MathTex(r'(1x^5', r'(ax^4', r'(bx^3', r'(cx^2', r'(dx^1', r'(ex^0').arrange(DOWN, aligned_edge = LEFT)
+        E4 = MathTex(r'=\ 1(z+h)^5', r'=\ a(z+h)^4', r'=\ b(z+h)^3', r'=\ c(z+h)^2', r'=\ d(z+h)^1', r'=\ e)').arrange(DOWN, aligned_edge = LEFT)
+
+        E5 = MathTex(
+            r'=\ 1(z^5 + 5hz^4 + 10h^2z^3 + 10h^3z^2 + 5h^4z + h^5)',
+            r'=\ a(z^4 + 4hz^3 + 6h^2z^2 + 4h^3z + h^4)',
+            r'=\ b(z^3 + 3hz^2 + 3h^2z + h^3)',
+            r'=\ c(z^2 + 2hz + h^2)',
+            r'=\ d(z + h)',
+            r'=\ e'
+        )
+
+        E6 = MathTex(
+            r'=\ 1z^5 + 5hz^4 + 10h^2z^3 + 10h^3z^2 + 5h^4z + h^5',
+            r'=\ az^4 + 4ahz^3 + 6ah^2z^2 + 4ah^3z + ah^4',
+            r'=\ bz^3 + 3bhz^2 + 3bh^2z + bh^3',
+            r'=\ cz^2 + 2chz + ch^2)',
+            r'=\ dz + dh',
+            r'=\ e'
+        )
 
         hide_chars([
-            Y[0][1],
-            E1[0][1], E1[4][3], E1[5][2], E1[5][3],
-            E3[0][1], E3[4][3], E3[5][2], E3[5][3],
+            Y1[0][0], Y1[0][1], Y1[0][3],
+            E1[0][1], E1[0][4], E1[1][4], E1[2][4], E1[3][4], E1[4][3], E1[4][4], E1[5][2], E1[5][3], E1[5][4],
+            E3[0][1], E3[0][4], E3[1][4], E3[2][4], E3[3][4], E3[4][3], E3[4][4], E3[5][2], E3[5][3], E3[5][4],
+            Y2[0][0], Y2[0][1], Y2[1][0], Y2[2][0], Y2[3][0], Y2[4][0], Y2[4][3], Y2[5][0], Y2[5][2], Y2[5][3],
+            E4[0][1], E4[4][7], E4[5][2]
             ])
 
         E2 = E1.copy().arrange(DOWN, aligned_edge = LEFT)
         G1 = VGroup(E1, E2).arrange(DOWN, aligned_edge = LEFT)
-        G2 = VGroup(Y, G1).arrange(RIGHT, aligned_edge = UP);
+        G2 = VGroup(Y1, Y2).arrange(DOWN, aligned_edge = LEFT)
+        G3 = VGroup(G2, G1).arrange(RIGHT, aligned_edge = UP);
 
-        self.play(FadeIn(Y))
+        self.play(FadeIn(Y1))
         self.play(Create(E1))
-        self.wait(3)
+        self.wait(pause)
+
         self.play(TransformFromCopy(E1, E2, path_arc = 2))
-        self.wait(3)
+        self.wait(pause)
 
+        E3.move_to(E1, aligned_edge = LEFT)
         self.play(Transform(E1, E3))
-        self.wait(3)
+        self.wait(pause)
+
+        self.play(TransformMatchingShapes(E2, Y2))
+        self.wait(pause)
+
+        E4.move_to(E2, aligned_edge = LEFT)
+        self.play(Create(E4))
+        self.wait(pause)
+
+        for index in range(6):
+            E5[index].move_to(E4[index], aligned_edge = LEFT)
+            self.play(TransformMatchingShapes(E4[index], E5[index]))
+            self.wait(pause)
+
+        for index in range(6):
+            E6[index].move_to(E5[index], aligned_edge = LEFT)
+            self.play(TransformMatchingShapes(E5[index], E6[index]))
+            self.wait(pause)
 
 
-class Quintic03(Scene):
+class Quintic02(Scene):
     def construct(self):
 
         e1z = r"= x^5"
@@ -126,7 +170,7 @@ class Quintic03(Scene):
         #self.play(ReplacementTransform(E8, E9))
         #self.wait(5)
 
-class Quintic01(Scene):
+class Quintic03(Scene):
     def construct(self):
 
         e1 = 'y = x^5 + ax^4 + bx^3 + cx^2 + dx + e'
