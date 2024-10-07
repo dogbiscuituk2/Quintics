f239ae24f8435b4eef4f3e004e295265caec9d58
John Kerr <john@jmkerr.com>
2024-10-07 16:48:10 +0100

    Soup's ready!

--- scene.py
+++ scene.py
@@ -89,13 +89,8 @@ class Quintic01(Scene):
             M3[0][35][0][3],
             M3[0][41][0][2], M3[0][41][0][3])
 
-        Z1 = Matrix([
-            ['(1^0'],
-            ['(1^0'],
-            ['(1^0'],
-            ['(1^0'],
-            ['(1^0'],
-            ['(1^0']], bracket_h_buff = 0)
+        z = ['(1^0']
+        Z1 = Matrix([z, z, z, z, z, z], bracket_h_buff = 0)
 
         for i in range(6):
             hide_chars(Z1[0][i][0][0], Z1[0][i][0][2])
@@ -184,136 +179,26 @@ class Quintic01(Scene):
         M = M3[0]
         Z = Z1[0]
 
+        z = [
+            ['1', '1'],
+            ['0', '5h', 'a'],
+            ['p', '10h^2', '4ah', 'b'],
+            ['q', '10h^3', '6ah^2', '3bh', 'c'],
+            ['r', '5h^4', '4ah^3', '3bh^2', '2ch', 'd']]
+
         def new_target(i, j):
             T = MathTex(f'z^{5 - i}' if i < 4 else 'z')
             T.move_to(M[i + 6 * j], RIGHT)
             T.generate_target()
-            T.target.move_to(Z[i], RIGHT)
+            T.target.move_to(Z[i], DOWN)
             return T
-        
-        def new_targets(i):
+
+        for i in range(5):
             T = [MoveToTarget(new_target(i, j)) for j in range(i + 2)]
             T.append(FadeOut(Z[i]))
             indicate([M[i + 6 * j] for j in range(i + 2)])
             self.play(*T)
+            self.play([replace(M[i + 6 * j], z[i][j]) for j in range(i + 2)])
 
-        new_targets(0)
-        self.play(
-            replace(M[0], '1'),
-            replace(M[6], '1'))
-
-        new_targets(1)
-        self.play(
-            replace(M[ 1], '0'),
-            replace(M[ 7], '5h'),
-            replace(M[13], 'a'))
-
-        new_targets(2)
-        self.play(
-            replace(M[ 2], 'p'),
-            replace(M[ 8], '10h^2'),
-            replace(M[14], '4ah'),
-            replace(M[20], 'b'))
-
-        new_targets(3)
-        self.play(
-            replace(M[ 3], 'q'),
-            replace(M[ 9], '10h^3'),
-            replace(M[15], '6ah^2'),
-            replace(M[21], '3bh'),
-            replace(M[27], 'c'))
-        
-        new_targets(4)
-        self.play(
-            replace(M[ 4], 'r'),
-            replace(M[10], '5h^4'),
-            replace(M[16], '4ah^3'),
-            replace(M[22], '3bh^2'),
-            replace(M[28], '2ch'),
-            replace(M[34], 'd'))
 
         self.wait(10)
-
-class Quintic02(Scene):
-    def construct(self):
-
-        e1z = r"= x^5"
-        e1a = r"+ ax^4"
-        e1b = r"+ bx^3"
-        e1c = r"cx^2"
-        e2d = r"+ dx"
-        e2e = r"+ e"
-
-        e1 = r"y = x^5 + ax^4 + bx^3 + cx^2 + dx + e"
-
-        E1 = VGroup(MathTex(e1))
-
-        self.play(Create(E1))
-        self.wait(3)
-
-        
-        e2 = r"y = (z+h)^5 + a(z+h)^4 + b(z+h)^3 + c(z+h)^2 + d(z+h) + e"
-        e3 = r"y = z^5 + 0z^4 + pz^3 + qz^2 + rz + s"
-
-        e4 = r"""
-             x^5 & =  (z+h)^5 =  z^5 +  5hz^4 + 10h^2z^3 + 10h^3z^2 + 5h^4z + h^5 \\
-            ax^4 & = a(z+h)^4 = az^4 + 4ahz^3 + 6ah^2z^2 + 4ah^3z   + ah^4 \\
-            bx^3 & = b(z+h)^3 = bz^3 + 3bhz^2 + 3bh^2z   +  bh^3 \\
-            cx^2 & = c(z+h)^2 = cz^2 + 2chz   +  ch^2 \\
-            dx   & = d(z+h)   = dz   +  dh \\
-            e    & = e
-        """
-        E1 = MathTex(e1)
-        E2 = MathTex(e2)
-        E3 = MathTex(e3)
-        E4 = MathTex(e4)
-
-        Y = r"\begin{pmatrix} y \\ \hline x^5 \\ ax^4 \\ bx^3 \\ cx^2 \\ dx \\ e \end{pmatrix}"
-        
-        M1 = r"""\begin{pmatrix} 
-            z^5 & 0z^4 & pz^3 & qz^2 & rz & s \\
-            \hline 
-            z^5 & 5hz^4 & 10h^2z^3 & 10h^3z^2 &  5h^4z &  h^5 \\ 
-             .  &  az^4 &   4ahz^3 & 6ah^2z^2 & 4ah^3z & ah^4 \\ 
-             .  &     . &     bz^3 &   3bhz^2 & 3bh^2z & bh^3 \\ 
-             .  &     . &        . &     cz^2 &   2chz & ch^2 \\ 
-             .  &     . &        . &        . &     dz & dh   \\ 
-             .  &     . &        . &        . &      . & e
-        \end{pmatrix}"""
-        
-        M2 = r"""\begin{pmatrix} 
-            1 & 0 & p & q & r & s \\
-            \hline 
-            1 & 5h & 10h^2 & 10h^3 &  5h^4 &  h^5 \\ 
-            . &  a &   4ah & 6ah^2 & 4ah^3 & ah^4 \\ 
-            . &  . &     b &   3bh & 3bh^2 & bh^3 \\ 
-            . &  . &     . &     c &   2ch & ch^2 \\ 
-            . &  . &     . &     . &     d & dh   \\ 
-            . &  . &     . &     . &     . & e  
-        \end{pmatrix}"""
-
-        Z1 = r"\begin{pmatrix} 1 \\ 1 \\ 1 \\ 1 \\ 1 \\ 1 \end{pmatrix}"
-        Z2 = r"\begin{pmatrix} z^5 \\ z^4 \\ z^3 \\ z^2 \\ z \\ 1 \end{pmatrix}"
-        
-        E8 = MathTex(Y, r"=", M1, Z1)
-        E9 = MathTex(Y, r"=", M2, Z2)
-        
-        g = Group(E1, E2, E3, E8).arrange(DOWN, aligned_edge = LEFT)
-
-        self.play(Create(E1))
-        self.wait(3)
-
-        #self.play(TransformFromCopy(E1, E2))
-        #self.wait(3)
-        #self.play(TransformFromCopy(E1, E3))
-        #self.wait(3)
-        #self.play(Create(E4))
-        #self.wait(3)
-
-        #self.play(Create(E8))
-        #self.wait(3)
-
-        #self.play(ReplacementTransform(E4, E8))
-        #self.wait(5)
-        #self.play(ReplacementTransform(E8, E9))
-        #self.wait(5)
