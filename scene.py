fa142af94babb785b73838d1fe647853a181a2e7
John Kerr <john@jmkerr.com>
2024-10-09 13:01:23 +0100

    lunch time

--- scene.py
+++ scene.py
@@ -3,7 +3,7 @@ from manim import *
 class Quintic01(Scene):
     def construct(self):
 
-        def get_colour(c):
+        def get_colour(c: str):
             if c.isnumeric():
                 return PINK
             match(c):
@@ -25,22 +25,24 @@ class Quintic01(Scene):
         def indicate(args):
             self.play(Indicate(VGroup(*args), color = WHITE))
 
-        def make_matrix(data, hb: float = 1.3, bhb: float = MED_SMALL_BUFF):
-            rows = len(data)
-            cols = len(data[0])
-            s = [[f'|{t}' for t in row] for row in data]
+        def make_matrix(matrix, hb: float = 1.3, bhb: float = MED_SMALL_BUFF):
+            rows = len(matrix)
+            cols = len(matrix[0])
+            s = [[f'|{t}' for t in row] for row in matrix]
             M = Matrix(s, h_buff = hb, bracket_h_buff = bhb)
             for row in range(rows):
                 for col in range(cols):
                     paint_tex(M[0][row * cols + col][0], s[row][col])
+            M[1].set_color(LIGHT_GREY)
+            M[2].set_color(LIGHT_GREY)
             return M
 
         def make_tex(*args):
             s = [f'|{arg}' for arg in args]
-            T = MathTex(*s)
+            t = MathTex(*s)
             for i in range(len(s)):
-                paint_tex(T[i], s[i])
-            return T
+                paint_tex(t[i], s[i])
+            return t
 
         def paint_tex(tex: MathTex, text: str):
             colour = BLACK
@@ -65,53 +67,53 @@ class Quintic01(Scene):
         def replace(S: MathTex, t: str):
             return ReplacementTransform(S, make_tex(t).move_to(S.get_center()))
 
-        y1 = 'y='
-        y2 = ['x^5=', 'ax^4=', 'bx^3=', 'cx^2=', 'dx=', 'e=']
-        e1 = ['x^5+', 'ax^4+', 'bx^3+', 'cx^2+', 'dx+', 'e']
-        e2 = ['z^5+', '0z^4+', 'pz^3+', 'qz^2+', 'rz+', 's']
-        e3 = ['(z+h)^5', 'a(z+h)^4', 'b(z+h)^3', 'c(z+h)^2', 'd(z+h)', 'e']
+        y1 = 'y^|='
+        y2 = ['x^5=', 'ax^4=', 'bx^3=', 'cx^2=', 'dx^|=', 'e^|=']
+        e1 = ['x^5+', 'ax^4+', 'bx^3+', 'cx^2+', 'dx^|+', 'e^|']
+        e2 = ['z^5+', '0z^4+', 'pz^3+', 'qz^2+', 'rz^|+', 's^|']
+        e3 = ['(z+h)^5', 'a(z+h)^4', 'b(z+h)^3', 'c(z+h)^2', 'd(z+h)^|', 'e^|']
         e4 = [
                 '(z^5+5hz^4+10h^2z^3+10h^3z^2+5h^4z+h^5)',
                 'a(z^4+4hz^3+6h^2z^2+4h^3z+h^4)',
                 'b(z^3+3hz^2+3h^2z+h^3)',
                 'c(z^2+2hz+h^2)',
-                'd(z+h)',
-                'e']
+                'd(z+h)^|',
+                'e^|']
         e5 = [
                 ['z^5+', '5hz^4+', '10h^2z^3+', '10h^3z^2+', '5h^4z+', 'h^5'],
                 ['az^4+', '4ahz^3+', '6ah^2z^2+', '4ah^3z+', 'ah^4'],
                 ['bz^3+', '3bhz^2+', '3bh^2z+', 'bh^3'],
                 ['cz^2+', '2chz+', 'ch^2'],
-                ['dz+', 'dh'],
-                ['e']]
+                ['dz^|+', 'dh^|'],
+                ['e^|']]
         y3 = [
-                ['y'],
+                ['y^|'],
                 ['x^5'],
                 ['ax^4'],
                 ['bx^3'],
                 ['cx^2'],
-                ['dx'],
-                ['e']]
+                ['dx^|'],
+                ['e^|']]
         m1 = [
-                ['z^5', '0z^4', 'pz^3', 'qz^2', 'rz', 's'],
+                ['z^5', '0z^4', 'pz^3', 'qz^2', 'rz^|', 's^|'],
                 ['z^5', '5hz^4', '10h^2z^3', '10h^3z^2', '5h^4z', 'h^5'],
                 ['', 'az^4', '4ahz^3', '6ah^2z^2', '4ah^3z', 'ah^4'],
                 ['', '', 'bz^3', '3bhz^2', '3bh^2z', 'bh^3'],
                 ['', '', '', 'cz^2', '2chz', 'ch^2'],
-                ['', '', '', '', 'dz', 'dh'],
-                ['', '', '', '', '', 'e']]
-        z0 = ['1']
+                ['', '', '', '', 'dz^|', 'dh^|'],
+                ['', '', '', '', '', 'e^|']]
+        z0 = ['|1|']
         z1 = [z0, z0, z0, z0, z0, z0]
         z = [
-                ['1', '0', 'p', 'q', 'r', 's'],
-                ['1', '5h', '10h^2', '10h^3', '5h^4', 'h^5'],
-                ['', 'a', '4ah', '6ah^2', '4ah^3', 'ah^4'],
-                ['', '', 'b', '3bh', '3bh^2', 'bh^3'],
-                ['', '', '', 'c', '2ch', 'ch^2'],
-                ['', '', '', '', 'd', 'dh'],
-                ['', '', '', '', '', 'e']]
+                ['1^|', '0^|', 'p^|', 'q^|', 'r^|', 's^|'],
+                ['1^|', '5h^|', '10h^2', '10h^3', '5h^4', 'h^5'],
+                ['', 'a^|', '4ah^|', '6ah^2', '4ah^3', 'ah^4'],
+                ['', '', 'b^|', '3bh^|', '3bh^2', 'bh^3'],
+                ['', '', '', 'c^|', '2ch^|', 'ch^2'],
+                ['', '', '', '', 'd^|', 'dh^|'],
+                ['', '', '', '', '', 'e^|']]
         e6 = [
-                ['0=5h+a', 'a=-5h'],
+                ['0=5h+a^|', 'a^|=-5h'],
                 ['p=10h^2-20h^2+b', 'p=b-10h^2'],
                 ['q=10h^3-30h^3+3bh+c', 'q=3bh+c-20h^3'],
                 ['r=5h^4-20h^4+3bh^2+2ch+d', 'r=3bh^2+2ch+d-15h^4'],
@@ -122,7 +124,7 @@ class Quintic01(Scene):
                 [ '0',   '0', '-20',   '0', '3b', 'c'],
                 [ '0', '-15',   '0',  '3b', '2c', 'd'],
                 ['-4',   '0',   'b',   'c',  'd', 'e']]
-        h4 = [['h^5'], ['h^4'], ['h^3'], ['h^2'], ['h'], ['1']]
+        h4 = [['h^5'], ['h^4'], ['h^3'], ['h^2'], ['h^|'], ['1^|']]
 
         Y1 = make_tex(y1)
         Y2 = make_tex(*y2).arrange(DOWN, aligned_edge = RIGHT)
@@ -206,6 +208,7 @@ class Quintic01(Scene):
 # Convert to matrix equation Y=MZ
 
         EQ = MathTex('=')
+        EQ.set_color(LIGHT_GREY)
         VGroup(Y3, EQ, M1, Z1).arrange(RIGHT, aligned_edge = UP)
         EQ.move_to(EQ.get_center() + 2.9 * DOWN)
         Z1.move_to(Z1.get_center() + 0.5 * DOWN)
@@ -220,22 +223,25 @@ class Quintic01(Scene):
         M = M1[0]
         Z = Z1[0]
 
-        def get_element(row, column):
-            return M[6 * row + column]
+        def get_element(row: int, col: int):
+            return M[6 * row + col]
 
-        def new_target(row, column):
-            T = make_tex(f'z^{5 - column}' if column < 4 else 'z')
-            T.move_to(get_element(row, column), RIGHT)
+        def new_target(row: int, col: int):
+            T = make_tex(f'z^{5 - col}' if col < 4 else 'z')
+            T.move_to(get_element(row, col), RIGHT)
             T.generate_target()
-            T.target.move_to(Z[column], DOWN)
+            T.target.move_to(Z[col], DOWN)
             return T
 
-        for column in range(5):
-            rows = range(column + 2)
-            T = [MoveToTarget(new_target(row, column)) for row in rows]
-            T.append(FadeOut(Z[column]))
-            T.append([replace(get_element(row, column), z[row][column]) for row in rows])
-            indicate([get_element(row, column) for row in rows])
+        for col in range(5):
+            rows = range(col + 2)
+            T = [MoveToTarget(new_target(row, col)) for row in rows]
+            T.append(FadeOut(Z[col]))
+            T.append([replace(get_element(row, col), z[row][col]) for row in rows])
+            indicate([get_element(row, col) for row in rows])
             self.play(*T)
+        pause()
+
+# Transpose the 
 
         self.wait(3)
