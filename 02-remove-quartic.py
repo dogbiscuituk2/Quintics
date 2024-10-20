04fadbde94069800f51703a135bbf597f65edcc0
John Kerr <john@jmkerr.com>
2024-10-20 14:51:53 +0100

    tomato soup

--- 02-remove-quartic.py
+++ 02-remove-quartic.py
@@ -37,9 +37,6 @@ class Quintic02(VoiceoverScene):
             [f'o{z9}'],
             [f'o{z9}', 'az^4+4ahz^3+6ah^2z^2+4ah^3z+ah^4', 'bz^3+3bhz^2+3bh^2z+bh^3', 'cz^2+2chz+ch^2', 'dz+dh', 'e']]]
 
-        for i in range(0, 9):
-            Fz[i][0][0][1].set_opacity(0)
-
         LHS = VGroup(*[make_tex(f'{s}o=') for s in ['x', 'y', 'y', 'x^5', 'ax^4', 'bx^3', 'cx^2', 'dx', 'e']])
         RHS = VGroup(*[make_tex(s) for s in ['oz+h', 'ox^5+ax^4+bx^3+cx^2+dx+e=0', 'oz^5+0z^4+pz^3+qz^2+rz+s']], *Fz[9])
         ALL = VGroup(LHS, RHS)
@@ -49,40 +46,64 @@ class Quintic02(VoiceoverScene):
         ALL.arrange(RIGHT, aligned_edge=DOWN)
         EQU = [VGroup(LHS[i], RHS[i]) for i in range(9)]
 
-        with self.voiceover(text="This is the General Form of a quintic polynomial equation in one variable, x.") as tracker:
-            say(tracker)
+        Y = make_matrix((['y'], ['x^5'], ['ax^4'], ['bx^3'], ['cx^2'], ['dx'], ['e']), margin = 0)
+        EQ = MathTex('=').set_color(Grey)
+        M = make_matrix((
+            ('z^5', '0z^4', 'pz^3', 'qz^2', 'rz', 's'),
+            ('z^5', '5hz^4', '10h^2z^3', '10h^3z^2', '5h^4z', 'h^5'),
+            ('', 'az^4', '4ahz^3', '6ah^2z^2', '4ah^3z', 'ah^4'),
+            ('', '', 'bz^3', '3bhz^2', '3bh^2z', 'bh^3'),
+            ('', '', '', 'cz^2', '2chz', 'ch^2'),
+            ('', '', '', '', 'dz', 'dh'),
+            ('', '', '', '', '', 'e')),
+            padding = 1.75)
+        Z = make_matrix((('1'), ('1'), ('1'), ('1'), ('1'), ('1')), margin = 0)
+        MEQ = VGroup(Y, EQ, M, Z).arrange(RIGHT, aligned_edge=DOWN)
+        EQ.move_to(EQ.get_center() + 2.50 * UP)
+        Z.move_to(Z.get_center() + 0.25 * UP)
+
+        with say(self, "This is the General Form of a quintic polynomial equation in one variable, x."):
             self.play(Create(EQU[1]))
             dump(*EQU)
 
-        with self.voiceover(text="We could solve it if we could remove all these pesky intermediate x powers.") as tracker:
-            say(tracker)
+        with say(self, "We could solve it easily if we didn't have all these intermediate x powers."):
             box(self, *EQU[1][1][0][4:19])
+            s1 = make_tex('y=x^5+e=0')
+            s2 = MathTex('x=\\sqrt[5]{-e}')
+            s2[0].color = Grey;
+            s2[0][0].color = Red;
+            s2[0][6].color = Green;
+            VGroup(s1, s2).arrange(DOWN)
+            self.play(
+                TransformMatchingShapes(EQU[1].copy(), s1),
+                Create(s2))
 
-        with self.voiceover(text="To make a start, we might first try to get rid of the quartic, or x to the fourth, term.") as tracker:
-            say(tracker)
+        with say(self, "To make a start, we might first try to get rid of the quartic, or x to the fourth, term."):
             box(self, *EQU[1][1][0][4:8])
+            self.play(Uncreate(s2))
+            self.play(Uncreate(s1))
 
-        with self.voiceover(text="In other words, transform it into so-called reduced form, where the coefficient of this term is zero.") as tracker:
-            say(tracker)
-            EQU[2][0][0][1].set_opacity(0)
+        with say(self, "In other words, transform it into so-called reduced form,"):
             self.play(Create(EQU[2]))
             box(self, *EQU[2][1][0][4:8])
             dump(*EQU)
 
-        with self.voiceover(text="This operation is technically known as a Tschirnhaus Transformation,") as tracker:
-            say(tracker)
-            Tschirnhaus = ImageMobject("resources/Tschirnhaus.jpg")
-            Tschirnhaus.to_corner(DR, buff=0)
-            self.play(FadeIn(Tschirnhaus))
+        with say(self, "where the coefficient of this term is zero."):
+            self.play(Indicate(EQU[2][1][0][5], color=WHITE, scale_factor=2, run_time=2))
+
+        with say(self, "This operation is technically known as a Tschirnhaus Transformation,"):
+            image = ImageMobject("resources/Tschirnhaus.jpg")
+            caption = MarkupText('Ehrenfried Walther von Tschirnhaus, 1651-1708', color=Yellow).scale(0.25)
+            picture = Group(image, caption).arrange(DOWN)
+            picture.to_corner(DR)
+            self.play(FadeIn(picture))
             unbox(self)
 
-        with self.voiceover(text="the simplest example of which is a linear substitution, such as x = z + some constant h.") as tracker:
-            say(tracker)
+        with say(self, "the simplest example of which is a linear substitution, such as x = z + some constant h."):
             box(self, EQU[0])
             self.play(Create(EQU[0]))
 
-        with self.voiceover(text="Let's use this to express all these x powers in terms of z.") as tracker:
-            say(tracker)
+        with say(self, "Let's use this to express all these x powers in terms of z."):
             E = EQU[1][1][0]
             S = [E[2:4], E[5:8], E[9:12], E[13:16], E[17:19], E[20:21]]
             T = LHS[3:9]
@@ -97,121 +118,49 @@ class Quintic02(VoiceoverScene):
             dump(*EQU)
 
         def expand(i: int, immediate: bool = False) -> None:
-            fz = Fz[i]
-            op_list = []
-            for j in range(1 if immediate else 0, len(fz)):
-                S = EQU[j+3][1]
-                T = fz[j]
-                T.move_to(S, aligned_edge=LEFT)
-                op = TransformMatchingShapes(S, T)
-                if immediate:
-                    self.play(op)
-                else:
-                    op_list.append(op)
-                EQU[j+3][1] = T
-            if not immediate:
-                self.play(op_list)
 
-        with self.voiceover(text="Expand these powers.") as tracker:
-            say(tracker)
+            def move_box(grow: int=0) -> Animation:
+                return box_move(*[EQU[j+3][1][0][k] for k in range(1, 6*(i-3)+grow) for j in range(0, 9-i)])
+
+            if i in range(5, 9):
+                self.play(move_box())
+            fz = Fz[i]
+            animations = []
+            for j in range(1 if immediate else 0, len(fz)):
+                source = EQU[j+3][1]
+                target = fz[j]
+                target.move_to(source, aligned_edge=LEFT)
+                animation = TransformMatchingShapes(source, target)
+                if immediate:
+                    self.play(animation)
+                else:
+                    animations.append(animation)
+                EQU[j+3][1] = target
+            if not immediate:
+                if i in range(5, 9):
+                    animations.append(move_box(1))
+                self.play(animations)
+
+        with say(self, "Expand these powers."):
+            self.play(FadeOut(picture))
             for i in range(1, 5):
                 expand(i)
                 dump(*EQU)
 
-        with self.voiceover(text="Now multiply out the binomials.") as tracker:
-            say(tracker)
+        with say(self, "Multiply out the binomials."):
             for i in range(5, 9):
                 expand(i)
                 dump(*EQU)
+            unbox(self)
 
-        with self.voiceover(text="Then distribute the original coefficients.") as tracker:
-            say(tracker)
+        with say(self, "Distribute the original coefficients."):
             expand(9, immediate=True)
             dump(*EQU)
 
-        with self.voiceover(text="This set of expressions.") as tracker:
-            say(tracker)
-            box(self, *[EQU[row][1] for row in range(2, 9)])
-
         self.wait(5)
 
-"""
-
-            #self.wait(2)
-
-        self.wait(10)
-        
-
-        
-        L = ('y', 'z', 'y', 'x^5', 'ax^4', 'bx^3', 'cx^2', 'dx', 'e')
-        Z = (
-            '1',
-            '(z+h)',
-            '(z^2+2hz+h^2)',
-            '(z^3+3hz^2+3h^2z+h^3)',
-            '(z^4+4hz^3+6h^2z^2+4h^3z+h^4)',
-            '(z^5+5hz^4+10h^2z^3+10h^3z^2+5h^4z+h^5)')
-
-        R7 = (
-            'x^5+ax^4+bx^3+cx^2+dx+e',
-            'x+h',
-            'z^5+0z^4+pz^3+qz^2+rz+s',
-
-            '(z+h)(z^4+4hz^3+6h^2z^2+4h^3z+h^4)',
-            'a(z+h)(z^3+3hz^2+3h^2z+h^3)',
-            'b(z+h)(z^2+2hz+h^2)',
-            'c(z+h)(z+h)',
-            'd(z+h)',
-            'e')
-        
-
-
-        R7 = (
-            'x^5+ax^4+bx^3+cx^2+dx+e',
-            'x+h',
-            'z^5+0z^4+pz^3+qz^2+rz+s',
-            Z[5], f'a({Z[4]})', f'b({Z[3]})', f'c({Z[2]})', f'd({Z[1]})', 'e')
-        R8 = (Z[5], f'a({Z[4]})', f'b({Z[3]})', f'c({Z[2]})', f'd({Z[1]})', 'e')
-        R9 = (
-            'z^5+5hz^4+10h^2z^3+10h^3z^2+5h^4z+h^5',
-            'az^4+4ahz^3+6ah^2z^2+4ah^3z+ah^4',
-            'bz^3+3bhz^2+3bh^2z+bh^3',
-            'cz^2+2chz+ch^2',
-            'dz+dh',
-            'e')
-        
-        LHS = VGroup(*[make_tex(f'{s}=') for s in L01])
-
-        e01 = 'x^5+ax^4+bx^3+cx^2+dx+e=0'
-        e02 = 'x+h'
-        e03a = 'z^5+0z^4+pz^3+qz^2+rz+s=0'
-        e03b = 'z^5+pz^3+qz^2+rz+s=0'
-        e04 = ('x^5+5hx^4+10h^2x^3+10h^3x^2+5h^4x+h^5', 'x^4', '', '', '')
-
-        LHS = VGroup(*[make_tex(f'{s}=') for s in ('y', 'z', 'y', 'x^5', 'ax^4', 'bx^3', 'cx^2', 'dx', 'e')])
-
-        RHS = VGroup(*[make_tex(s) for s in (
-        )])
-
-
-        RHS = VGroup(*[make_tex(s) for s in (e01, e02, e03a, )])
-            make_tex(e01),
-            make_tex(e02),
-            make_tex(e03a),
-            make_tex(e04)) \
-            .arrange(DOWN, aligned_edge=LEFT)
-        
-        G01 = VGroup(LHS, RHS).arrange(RIGHT, aligned_edge=DOWN)
-
-
-        with self.voiceover(text="This operation is called a Tschirnhaus Transform,") as tracker:
-            self.play(Indicate(E03[1][0][4]))
-            self.remove(box)
-            self.play(TransformMatchingShapes(E03[1], make_tex(e03b)))
-
-        with self.voiceover(text="the simplest example of which is a linear substitution, such as z = x + some constant h.") as t:
-            self.play(Create(E02))
-
+        self.play(FadeOut(EQU[0]), FadeOut(EQU[1]))
+        self.play(TransformMatchingShapes(VGroup(*[EQU[i][0] for i in range(2, 9)]), Y), Create(EQ))
+        self.play(TransformMatchingShapes(VGroup(*[EQU[i][1] for i in range(2, 9)]), M), Create(Z))
+            
         self.wait(5)
-
-        """
--- common.py
+++ common.py
@@ -1,9 +1,40 @@
-from manim import *
+import re
+
+from typing import Generator
+
+from manim_voiceover.services.azure import AzureService
+from manim_voiceover.services.base import SpeechService
 from manim_voiceover.services.gtts import GTTSService
 from manim_voiceover.tracker import VoiceoverTracker
 
+from manim import *
+
 config.max_files_cached = 999
 config.verbosity = 'WARNING'
+
+#region Box
+
+_box = None
+
+def box(self: Scene, *args: VMobject) -> None:
+    self.play(box_move(*args))
+
+def box_move(*args: VMobject) -> Animation:
+    global _box
+    b = SurroundingRectangle(VGroup(*args), Yellow) 
+    result = Create(b) if _box == None else ReplacementTransform(_box, b)
+    _box = b
+    return result
+
+def unbox(self: Scene) -> None:
+    global _box
+    if _box != None:
+        self.play(Uncreate(_box))
+        _box = None
+
+#endregion Box
+
+#region Colours
 
 PALETTE_DEFAULT = 0
 PALETTE_BRIGHT = 1
@@ -18,68 +49,29 @@ colours = (
     (0xFFFFFF, *[0x000000 for _ in range(12)]),
     (0x000000, *[0xFFFFFF for _ in range(12)]))
 
-#region box
-
-def box(self, *args):
-    global _box
-    b = SurroundingRectangle(VGroup(*args), Yellow) 
-    self.play(Create(b) if _box == None else ReplacementTransform(_box, b))
-    _box = b
-
-def unbox(self):
-    global _box
-    if _box != None:
-        self.play(Uncreate(_box))
-        _box = None
-
-#endregion
-
-def dump(*args) -> None:
-    for arg in args:
-        print([s.tex_string for s in arg])
-    print()
-
 def get_colour(char: str) -> ManimColor:
     for map in ColourMap:
         if (char in map[0]):
             return map[1]
     return Grey
 
-def init(self):
-    self.set_speech_service(GTTSService())
-
-def make_tex(*items: str) -> MathTex:
-    s: str = [prepare_string(item) for item in items]
-    mathTex: MathTex = MathTex(*s)
-    for i in range(len(s)):
-        paint_tex(mathTex[i], s[i])
-    return mathTex
-
 def paint_tex(mathTex: MathTex, s: str) -> None:
     colour = Black
     p = 0
-    super = False
-    for t in s.split('^'):
+    escape = False
+    #for t in s.split('^'):
+    for t in re.split('[_^]', s):
         for c in t:
             m = mathTex[p]
             if c in '|o':
                 m.set_opacity(0)
             else:
-                if not super:
+                if not escape:
                     colour = get_colour(c)
                 m.set_color(colour)
-            super = False
+            escape = False
             p += 1
-        super = True
-
-def prepare_string(s: str) -> str:
-    if not '|' in s: s = f'|{s}'
-    if not '^' in s: s = f'{s}^|'
-    return s
-
-def say(tracker: VoiceoverTracker) -> None:
-    print(tracker.data['input_text'])
-    print()
+        escape = True
 
 def set_colour_map(colour_map: tuple[tuple[str, ManimColor]]) -> None:
     global ColourMap
@@ -101,4 +93,63 @@ Violet      = colours[palette][10]
 Grey        = colours[palette][11]
 White       = colours[palette][12]
 
-_box = None
+#endregion Colours
+
+#region Debug
+
+def dump(*args) -> None:
+    for arg in args:
+        print([s.tex_string for s in arg])
+    print()
+
+#endregion Debug
+
+#region Init
+
+def init(self):
+    self.set_speech_service(speech_services[1])
+
+#endregion Init
+
+#region Speech
+
+speech_services: List[SpeechService] = [
+    AzureService(voice="en-US-AriaNeural", style="newscast-casual", global_speed=1.15),
+    GTTSService(())]
+
+def say(self, text: str): # -> Generator[VoiceoverTracker, None, None]:
+    tracker = self.voiceover(text)
+    #print(tracker.data['input_text'])
+    print(text)
+    print()
+    return tracker
+
+#endregion
+
+#region Text
+
+def make_matrix(matrix: List[List[str]], margin: float = MED_SMALL_BUFF, padding: float = 1.3) -> Matrix:
+    rows: int = len(matrix)
+    cols: int = len(matrix[0])
+    strings: List[str] = [[prepare_string(t) for t in row] for row in matrix]
+    matrix: Matrix = Matrix(strings, bracket_h_buff = margin, h_buff = padding)
+    for row in range(rows):
+        for col in range(cols):
+            paint_tex(matrix[0][row * cols + col][0], strings[row][col])
+    matrix[1].set_color(Grey)
+    matrix[2].set_color(Grey)
+    return matrix
+
+def make_tex(*items: str) -> MathTex:
+    s: str = [prepare_string(item) for item in items]
+    mathTex: MathTex = MathTex(*s)
+    for i in range(len(s)):
+        paint_tex(mathTex[i], s[i])
+    return mathTex
+
+def prepare_string(s: str) -> str:
+    if not '|' in s: s = f'|{s}'
+    if not '^' in s: s = f'{s}^|'
+    return s
+
+#endregion Text
