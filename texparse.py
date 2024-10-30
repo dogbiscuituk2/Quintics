ead19ee0e5c65fc1e15f86ae0b5f1f22e53a8959
John Kerr <john@jmkerr.com>
2024-10-30 18:15:09 +0000

    mid

--- texparse.py
+++ texparse.py
@@ -8,67 +8,73 @@ class TexParse():
     TexIndex: int = 0
     Tokens: List[str] = []
 
-    def accept(self, token):
-        self.pop()
+    def parse(self, mathTex: MathTex):
 
-    def begin(self, what: str):
-        self.debug(True, what)
+        def accept(token):
+            pop()
 
-    def block(self):
-        self.begin('block')
-        self.accept('{')
-        self.expression()
-        self.end('block')
+        def begin(what: str):
+            debug(0, what)
 
-    def debug(self, begin: bool, what: str) -> None:
-        if (not begin):
-            self.Level -= 1
-        print(f"{self.Level * ' '}{'begin' if begin else 'end'} {what}")
-        if (begin):
-            self.Level += 1
+        def block():
+            begin('block')
+            accept('{')
+            expression()
+            end('block')
 
-    def end(self, what: str):
-        self.debug(False, what)
+        def debug(stage_index: int, what: str) -> None:
+            if (stage_index > 0):
+                self.Level -= 1
+            print(f"{self.Level * ' '}{['begin', 'mid', 'end'][stage_index]} {what}")
+            if (stage_index < 2):
+                self.Level += 1
 
-    def expression(self):
-        self.begin('expression')
-        while self.Tokens:
-            token = self.pop()
-            match token[0]:
-                case '{':
-                    self.expression()
-                case '}':
-                    #print('end expression')
-                    break
-                case '\\':
-                    self.function(token)
+        def end(what: str):
+            debug(2, what)
+
+        def expression():
+            begin('expression')
+            while self.Tokens:
+                token = pop()
+                match token[0]:
+                    case '{':
+                        expression()
+                    case '}':
+                        break
+                    case '\\':
+                        function(token)
+                    case _:
+                        pass
+            end('expression')
+
+        def function(token):
+            name = token[1:]
+            foo = f"function {name}"
+            begin(foo)
+            match(name):
+                case 'frac':
+                    block()
+                    mid(foo)
+                    block()
+                case 'sqrt':
+                    pass
                 case _:
                     pass
-        self.end('expression')
+            end(foo)
 
-    def function(self, token):
-        self.begin(f"function {token}")
-        match(token):
-            case '\\frac':
-                self.block()
-                self.block()
-            case '\\sqrt':
-                pass
-            case _:
-                pass
-        self.end(f"function {token}")
+        def mid(what: str):
+            debug(1, what)
 
-    def parse(self, mathTex: MathTex):
+        def pop() -> str:
+            return self.Tokens.pop(0)
+
+        def touch(token):
+            # Process an element of the MathTex
+            # Point to the next MathTex element
+            print(f"parse_touch('{token}')")
+
         self.Tex = mathTex
         self.TexIndex = 0
         token_pattern = r"\\\w+|\{|\}|[^\\\{\}]"
         self.Tokens = re.findall(token_pattern, self.Tex.tex_string) 
-        self.expression()
-
-    def pop(self) -> str:
-        return self.Tokens.pop(0)
-
-    def touch(self, token):
-        # Process an element of the MathTex
-        # Point to the next MathTex element
-        print(f"parse_touch('{token}')")
+        expression()
