c22faddce69a51a455d52bdb1662d6f9fe40fca9
John Kerr <john@jmkerr.com>
2024-11-10 12:51:39 +0000

    remembrance

--- polynomials.py
+++ polynomials.py
@@ -1,4 +1,5 @@
 from contextlib import _GeneratorContextManager
+from inspect import currentframe, getframeinfo
 from manim import *
 from manim_voiceover import VoiceoverScene
 from manim_voiceover.services.gtts import GTTSService
@@ -77,10 +78,11 @@ class Polynomials(VoiceoverScene):
         return tex
 
     #def make_tex(self, *items: str) -> MathTex:
-    #    mathTex: MathTex = MathTex(*items)
-    #    for i in range(len(items)):
-    #        self.paint_tex(mathTex[i])
-    #    return mathTex
+    #    result = MathTex(*[self.prepare_string(item) for item in items])
+    #    self.paint(result)
+    #    return result
+    #
+    #    return [self.paint(MathTex(self.prepare_string(item))) for item in items]
 
     def make_text(self, text: str, *args, **kwargs) -> Text:
         result = Text(self.prepare_string(text), font_size=24, color=self.get_text_colour(), *args, **kwargs)
@@ -94,7 +96,8 @@ class Polynomials(VoiceoverScene):
         return text if '|' in text else f'|{text}'
 
     def say(self, text: str):
-        print(text)
+        line = getframeinfo(currentframe().f_back).lineno
+        print(f"{line}: {text}")
         # Specify language & disable language check to avoid GTTS bugs.
         return self.voiceover(text, lang='en', lang_check=False)
     
@@ -149,30 +152,62 @@ class Polynomials(VoiceoverScene):
             self.play(TransformByGlyphMap(F4, F5, ([], [36, 37])))
 
         with self.say("To find these values, start by dividing throughout by a n."):
-            self.flash(F5[0][37])
-            F6a = MathTex(r'\intertext{Let }').set_color(self.get_text_colour())
-            F6b = self.make_tex(r'b_i=a_i/a_n:')
-            F6c = VGroup(F6a, F6b).arrange(RIGHT, aligned_edge=UP)
-            F6c.next_to(F5, UP, aligned_edge=LEFT)
-            self.play(FadeOut(F2), FadeOut(F3), Create(F6c))
-            F6d = self.make_tex(r'y=x^n+b_{n-1}x^{n-1}+b_{n-2}x^{n-2}+...+b_1x+b_0=0')
-            self.play(TransformByGlyphMap(F5, F6d, ([3,4], ShrinkToCenter)))
+
+            self.flash(F5[0][37]) # The zero on the RHS
+
+            F6a = self.make_tex(r'\frac{y}{a_n}=\frac{a_n}{a_n}x^n+\frac{a_{n-1}}{a_n}x^{n-1}+\frac{a_{n-2}}{a_n}x^{n-2}+...+\frac{a_1}{a_n}x+\frac{a_0}{a_n}=0')
+            self.play(TransformByGlyphMap(
+                F5,
+                F6a,
+                (GrowFromCenter, [2,8,18,30,44,51]),
+                (FadeIn, [3,4,9,10,19,20,31,32,45,46,52,53]),
+                introduce_individually=True))
+
+            F6b = MathTex(r'\intertext{Let }').set_color(self.get_text_colour())
+            F6c = self.make_tex(r'b_i=a_i/a_n:')
+            F6d = VGroup(F6b, F6c).arrange(RIGHT, aligned_edge=UP)
+            F6d.next_to(F6a, UP, aligned_edge=LEFT)
+            self.play(FadeOut(F2), FadeOut(F3), Create(F6d))
+
+            F6e = self.make_tex( \
+                r'\frac{a_n}{a_n}x^n+\frac{a_{n-1}}{a_n}x^{n-1}+\frac{a_{n-2}}{a_n}x^{n-2}+...+' \
+                    r'\frac{a_1}{a_n}x+\frac{a_0}{a_n}=0')
+            self.play(TransformByGlyphMap(F6a, F6e, ([*ir(1,5)], ShrinkToCenter)), run_time=1.5)
+
+            F6f = self.make_tex( \
+                r'x^n+\frac{a_{n-1}}{a_n}x^{n-1}+\frac{a_{n-2}}{a_n}x^{n-2}+...+' \
+                    r'\frac{a_1}{a_n}x+\frac{a_0}{a_n}=0')
+            self.play(TransformByGlyphMap(F6e, F6f, ([*ir(1,5)], ShrinkToCenter)), run_time=1.5)
+
+            F6g = self.make_tex( \
+                r'x^n+b_{n-1}x^{n-1}+\frac{a_{n-2}}{a_n}x^{n-2}+...+' \
+                    r'\frac{a_1}{a_n}x+\frac{a_0}{a_n}=0')
+            self.play(TransformByGlyphMap(F6f, F6g, ([8,9,10], ShrinkToCenter)), run_time=1.5)
+
+            F6h = self.make_tex(r'x^n+b_{n-1}x^{n-1}+b_{n-2}x^{n-2}+...+b_1x+b_0=0')
+            self.play(
+                TransformByGlyphMap(
+                    F6g,
+                    F6h,
+                    ([17,18,19,31,32,33,38,39,40], ShrinkToCenter),
+                    remove_individually=True),
+                run_time=1.5)
 
         with self.say("This is called the monic form, where the coefficient of the highest power of x is equal to one."):
-            self.flash(F6b[0][7:9])
+            self.flash(F6c[0][7:9])
 
         with self.say("In fact this is often called the general form, as it has all the same roots as the original."):
-            self.flash(VGroup(*F6d[0][3:5]))
-            self.play(FadeOut(F6c))
+            self.flash(VGroup(*F6a[0][3:5]))
+            self.play(FadeOut(F6d))
 
         with self.say("If we know these roots, we can express the polynomial as a product of n linear factors."):
             F7 = self.make_tex(r'=\prod_{j=1}^{n}(x-x_j)')
-            F7.next_to(F6d, DOWN)
+            F7.next_to(F6a, DOWN)
             self.play(Create(F7))
 
         with self.say("Each factor corresponds to a root value of the polynomial."):
             F8 = self.make_tex(r'=(x-x_1)(x-x_2)(x-x_3)...(x-x_{n-1})(x-x_n)')
-            F8.next_to(F6d, DOWN)
+            F8.next_to(F6a, DOWN)
             arc = {"path_arc": PI}
             self.play(
                 TransformByGlyphMap(
@@ -190,33 +225,35 @@ class Polynomials(VoiceoverScene):
             for g in ((5,7), (11,13), (17,19), (26,30), (34,36)):
                 self.flash(VGroup(*F8[0][g[0]:g[1]]), run_time=1)
             self.wait(2)
-            self.play(FadeOut(F6d, F8))
+            self.play(FadeOut(F6a, F8))
             self.wait(2)
 
 #endregion
 #region Scene 2 : Quintic 
 
-        self.set_colour_map((
-            ('abcde', green),
-            ('h', orange),
-            ('pqrs', yellow),
-            ('x', red),
-            ('y', magenta),
-            ('z', cyan)))
-
-        E1 = self.make_tex(r'y=\sum_{i=0}^{5}a_ix^i=0')
-        E1a = MathTex(r'Degree=n=5').set_color(self.get_text_colour())
-        E1b = self.make_tex(r'y=x^5+ax^4+bx^3+cx^2+dx+e')
-
-        G = VGroup(E1, E1a, E1b).arrange(DOWN)
+        # from: r'x^n+b_{n-1}x^{n-1}+b_{n-2}x^{n-2}+...+b_1x+b_0=0'
+        # to:   r'y=x^5+ax^4+bx^3+cx^2+dx+e'
 
         with self.say("The degree five polynomial, the quintic, has five roots."):
-            self.play(Create(E1))
-            self.play(Create(E1a))
-            self.play(Create(E1b))
+            E1b = self.make_tex(r'y=x^5+ax^4+bx^3+cx^2+dx+e')
+            self.play(
+                TransformByGlyphMap(
+                    F6h,
+                    E1b,
+                    (GrowFromCenter, [1,2]),
+                    ([5,6,7], ShrinkToCenter),
+                    ([9,10,11], [8]),
+                    ([14,15,16], ShrinkToCenter),
+                    ([18,19,20], [12]),
+                    ([22,23,24,25], ShrinkToCenter),
+                    (GrowFromCenter, [14,15,16,17]),
+                    ([27], ShrinkToCenter),
+                    ([31,32,33], ShrinkToCenter)
+                    ))
 
-            self.wait(2)
-            self.play(FadeOut(G))
+            self.wait(10)
+
+        return
 
         z = '(z+h)'
         z2 = f'{z}{z}'
@@ -384,11 +421,10 @@ class Polynomials(VoiceoverScene):
             mathTex.target.move_to(Z[0][col], DOWN)
             return mathTex
 
-        def replace(src: MathTex, tgt: MathTex) -> Transform:
-            print(f"src: {src.tex_string}, tgt: {tgt.tex_string}")
+        def rewrite(src: MathTex, tgt: MathTex) -> Transform:
             M2.append(tgt)
             s = src.tex_string.replace('^', '')
-            i = s.index('z')
+            i = s.find('z')
             if i == 1:
                 maps = [([1,2], ShrinkToCenter), (GrowFromCenter, [1])]
             else:
@@ -408,18 +444,16 @@ class Polynomials(VoiceoverScene):
             for col in range(5):
                 transforms: List[Transform] = []
                 rows = range(col + 2)
-                if col < 5:
-                    transforms = [MoveToTarget(new_target(row, col)) for row in rows]
-                    transforms.append(FadeOut(Z[0][col]))
+                transforms = [MoveToTarget(new_target(row, col)) for row in rows]
+                transforms.append(FadeOut(Z[0][col]))
                 for row in rows:
-                    transforms.append(replace(get_element(row, col), self.make_tex(m2[row][col])))
-                if col < 5:
-                    indicate([get_element(row, col) for row in rows])
+                    transforms.append(rewrite(get_element(row, col), self.make_tex(m2[row][col])))
+                indicate([get_element(row, col) for row in rows])
                 self.play(*transforms)
+            for row in range(7):
+                M2.append(self.make_tex(m2[row][5]))
 
         self.wait(10)
-
-        return
 
         F1 = self.make_tex('y=x^5+ax^4+bx^3+cx^2+dx+e')
         F2 = self.make_tex('y=z^5+0z^4+pz^3+qz^2+rz+s')
@@ -427,7 +461,7 @@ class Polynomials(VoiceoverScene):
         F4 = self.make_tex('z=x+a/5')
 
         def setup(*args: str) -> MathTex:
-            return self.make_tex(*args).arrange(DOWN, aligned_edge = LEFT).move_to(2 * LEFT + DOWN)
+            return VGroup(*[self.make_tex(arg) for arg in args]).arrange(DOWN, aligned_edge = LEFT).move_to(2 * LEFT + DOWN)
 
         F5 = setup('0=5h+a', 'p=10h^2+4ah+b'  , 'q=10h^3+6ah^2+3bh+c', 'r=5h^4+4ah^3+3bh^2+2ch+d'   , 's=h^5+a^4+bh^3+ch^2+dh+e')
         F6 = setup('a=-5h' , 'p=10h^2-20h^2+b', 'q=10h^3-30h^3+3bh+c', 'r=5h^4-20h^4+3bh^2+2ch+d'   , 's=h^5-5h^5+bh^3+ch^2+dh+e')
@@ -438,16 +472,22 @@ class Polynomials(VoiceoverScene):
         with self.say("Now we can read the matrix column by column, to get expressions for the new coefficients in terms of the old."):
             self.play(FadeOut(Y, EQ, M, M2[0], M2[1], Z[0][5], Z[1], Z[2], *Z2))
             for f in (F5, F6, F7, F8, F9):
-                Group(F1, F2, F3, f).arrange(DOWN, aligned_edge = LEFT)
-            Group(F1, F2, F4, F8).arrange(DOWN, aligned_edge = LEFT)
+                VGroup(F1, F2, F3, f).arrange(DOWN, aligned_edge = LEFT)
+            VGroup(F1, F2, F4, F8).arrange(DOWN, aligned_edge = LEFT)
+
             M6 = [
                 VGroup(*[M2[i] for i in range(2, 5)]),
                 VGroup(*[M2[i] for i in range(5, 9)]),
                 VGroup(*[M2[i] for i in range(9, 14)]),
                 VGroup(*[M2[i] for i in range(14, 20)]),
                 VGroup(*[M2[i] for i in range(20, 27)])]
-            for i in range(5):
+            for i in range(4):
                 self.play(TransformMatchingShapes(M6[i], F5[i], path_arc=PI/2))
+
+        for i in enumerate(F5):
+            print(i);
+        
+        return
 
         with self.say(
             """
@@ -547,23 +587,23 @@ class Polynomials(VoiceoverScene):
             ('y', magenta),
             ('z', cyan)))
 
-        E1 = self.make_tex(r'y=\sum_{i=0}^{0}a_ix^i=a_0=0')
-        E1a = MathTex(r'Degree=n=0').set_color(self.get_text_colour())
-        E1a.next_to(E1, DOWN)
+        E1z = self.make_tex(r'y=\sum_{i=0}^{0}a_ix^i=a_0=0')
+        E1y = MathTex(r'Degree=n=0').set_color(self.get_text_colour())
+        E1y.next_to(E1z, DOWN)
         E1b = self.make_tex(r'a_n\neq{0}')
-        E1b.next_to(E1a, DOWN)
+        E1b.next_to(E1y, DOWN)
 
         with self.say("The degree zero polynomial has no roots."):
-            self.play(Create(E1))
+            self.play(Create(E1z))
 
         with self.say("Notice that the equation, y equals zero, can have no solutions."):
-            self.play(Create(E1a))
+            self.play(Create(E1y))
 
         with self.say("This is because a n is constrained to be both zero and nonzero.") as tracker:
             self.play(Create(E1b))
-            boxes = [self.box(E1b), self.box(E1a[0][7:10]), self.box(E1[0][13:17])]
+            boxes = [self.box(E1b), self.box(E1y[0][7:10]), self.box(E1z[0][13:17])]
             self.wait(tracker.duration + 2)
-            self.play(FadeOut(E1, E1a, E1b, *boxes))
+            self.play(FadeOut(E1z, E1y, E1b, *boxes))
             self.wait(2)
 
 #endregion
@@ -577,21 +617,21 @@ class Polynomials(VoiceoverScene):
             ('y', magenta),
             ('z', cyan)))
 
-        E1 = self.make_tex(r'y=\sum_{i=0}^{1}a_ix^i=0')
-        E1a = MathTex(r'Degree=n=1').set_color(self.get_text_colour())
-        E1a.next_to(E1, DOWN)
-        E1b = self.make_tex(r'a_1x+a_0=0').next_to(E1a, DOWN)
-        E1c = self.make_tex(r'a_1x=-a_0').next_to(E1a, DOWN)
-        E1d = self.make_tex(r'x=-a_0/a_1').next_to(E1a, DOWN)
-        E1e = self.make_tex(r'x_1=-a_0/a_1').next_to(E1a, DOWN)
+        E1z = self.make_tex(r'y=\sum_{i=0}^{1}a_ix^i=0')
+        E1y = MathTex(r'Degree=n=1').set_color(self.get_text_colour())
+        E1y.next_to(E1z, DOWN)
+        E1b = self.make_tex(r'a_1x+a_0=0').next_to(E1y, DOWN)
+        E1c = self.make_tex(r'a_1x=-a_0').next_to(E1y, DOWN)
+        E1d = self.make_tex(r'x=-a_0/a_1').next_to(E1y, DOWN)
+        E1e = self.make_tex(r'x_1=-a_0/a_1').next_to(E1y, DOWN)
 
         with self.say(
             """
             The degree one polynomial has a single root because  
             the equation y equals zero has one solution.
             """):
-            self.play(Create(E1))
-            self.play(Create(E1a))
+            self.play(Create(E1z))
+            self.play(Create(E1y))
 
         with self.say("Solving it is easy."):
             self.play(Create(E1b))
@@ -618,7 +658,7 @@ class Polynomials(VoiceoverScene):
             self.wait(1)
             self.play(TransformByGlyphMap(E1d, E1e, (GrowFromCenter, [2])))
             self.wait(2)
-            self.play(FadeOut(E1, E1a, E1e))
+            self.play(FadeOut(E1z, E1y, E1e))
             self.wait(2)
 
 #endregion
@@ -632,23 +672,23 @@ class Polynomials(VoiceoverScene):
             ('y', magenta),
             ('z', cyan)))
 
-        E1 = self.make_tex(r'y=\sum_{i=0}^{2}a_ix^i=0')
-        E1a = MathTex(r'Degree=n=2').set_color(self.get_text_colour())
+        E1z = self.make_tex(r'y=\sum_{i=0}^{2}a_ix^i=0')
+        E1y = MathTex(r'Degree=n=2').set_color(self.get_text_colour())
         E1b = self.make_tex(r'y=ax^2+bx+c')
         E1c = self.make_tex(r'x=\frac{-b\pm\sqrt{b^2-4ac}}{2a}')
 
-        VGroup(E1, E1a, E1b, E1c).arrange(DOWN)
+        VGroup(E1z, E1y, E1b, E1c).arrange(DOWN)
 
         with self.say("The degree two polynomial, the quadratic, has two roots."):
-            self.play(Create(E1))
-            self.play(Create(E1a))
+            self.play(Create(E1z))
+            self.play(Create(E1y))
 
         with self.say("It's usually solved directly, using this formula, without conversion to the monic form."):
             self.play(Create(E1b))
             self.play(Create(E1c))
 
             self.wait(2)
-            self.play(FadeOut(E1, E1a, E1b, E1c))
+            self.play(FadeOut(E1z, E1y, E1b, E1c))
             self.wait(2)
 
 #endregion
@@ -662,15 +702,15 @@ class Polynomials(VoiceoverScene):
             ('y', magenta),
             ('z', cyan)))
 
-        E1 = self.make_tex(r'y=\sum_{i=0}^{3}a_ix^i=0')
-        E1a = MathTex(r'Degree=n=3').set_color(self.get_text_colour())
+        E1z = self.make_tex(r'y=\sum_{i=0}^{3}a_ix^i=0')
+        E1y = MathTex(r'Degree=n=3').set_color(self.get_text_colour())
         E1b = self.make_tex(r'y=x^3+ax^2+bx+c')
 
-        G = VGroup(E1, E1a, E1b).arrange(DOWN)
+        G = VGroup(E1z, E1y, E1b).arrange(DOWN)
 
         with self.say("The degree three polynomial, the cubic, has three roots."):
-            self.play(Create(E1))
-            self.play(Create(E1a))
+            self.play(Create(E1z))
+            self.play(Create(E1y))
             self.play(Create(E1b))
 
             self.wait(2)
@@ -687,15 +727,15 @@ class Polynomials(VoiceoverScene):
             ('y', magenta),
             ('z', cyan)))
 
-        E1 = self.make_tex(r'y=\sum_{i=0}^{4}a_ix^i=0')
-        E1a = MathTex(r'Degree=n=4').set_color(self.get_text_colour())
+        E1z = self.make_tex(r'y=\sum_{i=0}^{4}a_ix^i=0')
+        E1y = MathTex(r'Degree=n=4').set_color(self.get_text_colour())
         E1b = self.make_tex(r'y=x^4+ax^3+bx^2+cx+d')
 
-        G = VGroup(E1, E1a, E1b).arrange(DOWN)
+        G = VGroup(E1z, E1y, E1b).arrange(DOWN)
 
         with self.say("The degree four polynomial, the quartic, has four roots."):
-            self.play(Create(E1))
-            self.play(Create(E1a))
+            self.play(Create(E1z))
+            self.play(Create(E1y))
             self.play(Create(E1b))
 
             self.wait(2)
