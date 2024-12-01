a7fa8397404020c73766ee8273f267641f82bd6e
John Kerr <john@jmkerr.com>
2024-12-01 16:25:31 +0000

    use displaytext

--- test_all.py
+++ test_all.py
@@ -39,7 +39,6 @@ VARIABLE_SIZED_SYMBOLS = [ # displayed formulae show larger version
     [r'\prod', r'\oint', r'\bigcap', r'\bigotimes', r'\bigwedge'],
     [r'\coprod', r'\iint', r'\bigcup', r'\bigodot', r'\bigsqcup'],
 ]
-
 STANDARD_FUNCTION_NAMES = [ # Function names should appear in Roman, not Italic.
     [r'\arccos', r'\arcsin', r'\arctan', r'\arg'],
     [r'\cos', r'\cosh', r'\cot', r'\coth'],
@@ -50,24 +49,25 @@ STANDARD_FUNCTION_NAMES = [ # Function names should appear in Roman, not Italic.
     [r'\min', r'\Pr', r'\sec', r'\sin'],
     [r'\sinh', r'\sup', r'\tan', r'\tanh'],
 ]
-
-BINARY_OPERATION_RELATION_SYMBOLS_1 = [
+BINARY_OPERATION_RELATION_SYMBOLS_1_A = [
     [r'\ast', r'\pm', r'\cap', r'\lhd'],
     [r'\star', r'\mp', r'\cup', r'\rhd'],
     [r'\cdot', r'\amalg', r'\uplus', r'\triangleleft'],
-    [r'\circ', r'\odot', r'\sqcap', r'\triangleright'], 
-    [r'\bullet', r'\ominus', r'\sqcup', r'\unlhd'], 
-    [r'\bigcirc', r'\oplus', r'\wedge', r'\unrhd'], 
-    [r'\diamond', r'\oslash', r'\vee', r'\bigtriangledown'], 
-    [r'\times', r'\otimes', r'\dagger', r'\bigtriangleup'], 
-    [r'\div', r'\wr', r'\ddagger', r'\setminus'], 
-    [r'\centerdot', r'\Box', r'\barwedge', r'\veebar'], 
-    [r'\circledast', r'\boxplus', r'\curlywedge', r'\curlyvee'], 
-    [r'\circledcirc', r'\boxminus', r'\Cap', r'\Cup'], 
-    [r'\circleddash', r'\boxtimes', r'\bot', r'\top'], 
+    [r'\circ', r'\odot', r'\sqcap', r'\triangleright'],
+    [r'\bullet', r'\ominus', r'\sqcup', r'\unlhd'],
+    [r'\bigcirc', r'\oplus', r'\wedge', r'\unrhd'],
+    [r'\diamond', r'\oslash', r'\vee', r'\bigtriangledown'],
+    [r'\times', r'\otimes', r'\dagger', r'\bigtriangleup'],
+    [r'\div', r'\wr', r'\ddagger', r'\setminus'],
+]
+BINARY_OPERATION_RELATION_SYMBOLS_1_B = [
+    [r'\centerdot', r'\Box', r'\barwedge', r'\veebar'],
+    [r'\circledast', r'\boxplus', r'\curlywedge', r'\curlyvee'],
+    [r'\circledcirc', r'\boxminus', r'\Cap', r'\Cup'],
+    [r'\circleddash', r'\boxtimes', r'\bot', r'\top'],
     [r'\dotplus', r'\boxdot', r'\intercal', r'\rightthreetimes'],
-    [r'\divideontimes', r'\square', r'\doublebarwedge', r'\leftthreetimes']]
-
+    [r'\divideontimes', r'\square', r'\doublebarwedge', r'\leftthreetimes'],
+]
 BINARY_OPERATION_RELATION_SYMBOLS_2 = [
     [r'\equiv', r'\leq', r'\geq', r'\perp'], 
     [r'\cong', r'\prec', r'\succ', r'\mid'], 
@@ -78,9 +78,9 @@ BINARY_OPERATION_RELATION_SYMBOLS_2 = [
     [r'\asymp', r'\sqsubset', r'\sqsupset', r'\rtimes'], 
     [r'\doteq', r'\sqsubseteq', r'\sqsupseteq', r'\smile'], 
     [r'\propto', r'\dashv', r'\vdash', r'\frown'], 
-    [r'\models', r'\in', r'\ni', r'\notin']]
-
-BINARY_OPERATION_RELATION_SYMBOLS_3 = [
+    [r'\models', r'\in', r'\ni', r'\notin'],
+]
+BINARY_OPERATION_RELATION_SYMBOLS_3_A = [
     [r'\approxeq', r'\leqq', r'\geqq', r'\lessgtr'],
     [r'\thicksim', r'\leqslant', r'\geqslant', r'\lesseqgtr'],
     [r'\backsim', r'\lessapprox', r'\gtrapprox', r'\lesseqqgtr'],
@@ -90,6 +90,8 @@ BINARY_OPERATION_RELATION_SYMBOLS_3 = [
     [r'\bumpeq', r'\eqslantless', r'\eqslantgtr', r'\backepsilon'],
     [r'\Bumpeq', r'\precsim', r'\succsim', r'\between'],
     [r'\doteqdot', r'\precapprox', r'\succapprox', r'\pitchfork'],
+]
+BINARY_OPERATION_RELATION_SYMBOLS_3_B = [
     [r'\thickapprox', r'\Subset', r'\Supset', r'\shortmid'],
     [r'\fallingdotseq', r'\subseteqq', r'\supseteqq', r'\smallfrown'],
     [r'\risingdotseq', r'\sqsubset', r'\sqsupset', r'\smallsmile'],
@@ -97,24 +99,26 @@ BINARY_OPERATION_RELATION_SYMBOLS_3 = [
     [r'\therefore', r'\curlyeqprec', r'\curlyeqsucc', r'\vDash'],
     [r'\because', r'\blacktriangleleft', r'\blacktriangleright', r'\Vvdash'],
     [r'\eqcirc', r'\trianglelefteq', r'\trianglerighteq', r'\shortparallel'],
-    [r'\neq', r'\vartriangleleft', r'\vartriangleright', r'\nshortparallel']]
-
-BINARY_OPERATION_RELATION_SYMBOLS_4 = [
-    [r'\ncong', r'\nleq', r'\ngeq', r'\nsubseteq'], 
-    [r'\nmid', r'\nleqq', r'\ngeqq', r'\nsupseteq'], 
-    [r'\nparallel', r'\nleqslant', r'\ngeqslant', r'\nsubseteqq'], 
-    [r'\nshortmid', r'\nless', r'\ngtr', r'\nsupseteqq'], 
-    [r'\nshortparallel', r'\nprec', r'\nsucc', r'\subsetneq'], 
-    [r'\nsim', r'\npreceq', r'\nsucceq', r'\supsetneq'], 
-    [r'\nVDash', r'\precnapprox', r'\succnapprox', r'\subsetneqq'], 
-    [r'\nvDash', r'\precnsim', r'\succnsim', r'\supsetneqq'], 
-    [r'\nvdash', r'\lnapprox', r'\gnapprox', r'\varsubsetneq'], 
-    [r'\ntriangleleft', r'\lneq', r'\gneq', r'\varsupsetneq'], 
-    [r'\ntrianglelefteq', r'\lneqq', r'\gneqq', r'\varsubsetneqq'], 
-    [r'\ntriangleright', r'\lnsim', r'\gnsim', r'\varsupsetneqq'], 
-    [r'\ntrianglerighteq', r'\lvertneqq', r'\gvertneqq']]
-
-ARROW_SYMBOLS1 = [
+    [r'\neq', r'\vartriangleleft', r'\vartriangleright', r'\nshortparallel'],
+]
+BINARY_OPERATION_RELATION_SYMBOLS_4_A = [
+    [r'\ncong', r'\nleq', r'\ngeq', r'\nsubseteq'],
+    [r'\nmid', r'\nleqq', r'\ngeqq', r'\nsupseteq'],
+    [r'\nparallel', r'\nleqslant', r'\ngeqslant', r'\nsubseteqq'],
+    [r'\nshortmid', r'\nless', r'\ngtr', r'\nsupseteqq'],
+    [r'\nshortparallel', r'\nprec', r'\nsucc', r'\subsetneq'],
+    [r'\nsim', r'\npreceq', r'\nsucceq', r'\supsetneq'],
+]
+BINARY_OPERATION_RELATION_SYMBOLS_4_B = [
+    [r'\nVDash', r'\precnapprox', r'\succnapprox', r'\subsetneqq'],
+    [r'\nvDash', r'\precnsim', r'\succnsim', r'\supsetneqq'],
+    [r'\nvdash', r'\lnapprox', r'\gnapprox', r'\varsubsetneq'],
+    [r'\ntriangleleft', r'\lneq', r'\gneq', r'\varsupsetneq'],
+    [r'\ntrianglelefteq', r'\lneqq', r'\gneqq', r'\varsubsetneqq'],
+    [r'\ntriangleright', r'\lnsim', r'\gnsim', r'\varsupsetneqq'],
+    [r'\ntrianglerighteq', r'\lvertneqq', r'\gvertneqq'],
+]
+ARROW_SYMBOLS_1 = [
     [r'\leftarrow', r'\longleftarrow', r'\uparrow'],
     [r'\Leftarrow', r'\Longleftarrow', r'\Uparrow'],
     [r'\rightarrow', r'\longrightarrow', r'\downarrow'],
@@ -122,16 +126,14 @@ ARROW_SYMBOLS1 = [
     [r'\leftrightarrow', r'\longleftrightarrow', r'\updownarrow'],
     [r'\Leftrightarrow', r'\Longleftrightarrow', r'\Updownarrow'],
 ]
-
-ARROW_SYMBOLS2 = [
+ARROW_SYMBOLS_2 = [
     [r'\mapsto', r'\longmapsto', r'\nearrow'],
     [r'\hookleftarrow', r'\hookrightarrow', r'\searrow'],
     [r'\leftharpoonup', r'\rightharpoonup', r'\swarrow'],
     [r'\leftharpoondown', r'\rightharpoondown', r'\nwarrow'],
     [r'\rightleftharpoons', r'\leadsto'],
 ]
-
-ARROW_SYMBOLS3 = [
+ARROW_SYMBOLS_3 = [
     [r'\dashrightarrow', r'\dashleftarrow', r'\leftleftarrows'],
     [r'\leftrightarrows', r'\Lleftarrow', r'\twoheadleftarrow'],
     [r'\leftarrowtail', r'\looparrowleft', r'\leftrightharpoons'],
@@ -144,13 +146,11 @@ ARROW_SYMBOLS3 = [
     [r'\Rsh', r'\downdownarrows', r'\upharpoonright'],
     [r'\downharpoonright', r'\rightsquigarrow'],
 ]
-
-ARROW_SYMBOLS4 = [
+ARROW_SYMBOLS_4 = [
     [r'\nleftarrow', r'\nrightarrow', r'\nLeftarrow'],
     [r'\nRightarrow', r'\nleftrightarrow', r'\nLeftrightarrow'],
 ]
-
-MISCELLANEUOS_SYMBOLS = [
+MISCELLANEUOS_SYMBOLS_1 = [
     [r'\infty', r'\forall', r'\Bbbk', r'\wp'],
     [r'\nabla', r'\exists', r'\bigstar', r'\angle'],
     [r'\partial', r'\nexists', r'\diagdown', r'\measuredangle'],
@@ -159,6 +159,8 @@ MISCELLANEUOS_SYMBOLS = [
     [r'\diamondsuit', r'\imath', r'\Finv', r'\triangledown'],
     [r'\heartsuit', r'\jmath', r'\Game', r'\triangle'],
     [r'\spadesuit', r'\ell', r'\hbar', r'\vartriangle'],
+]
+MISCELLANEUOS_SYMBOLS_2 = [
     [r'\cdots', r'\iiiint', r'\hslash', r'\blacklozenge'],
     [r'\vdots', r'\iiint', r'\lozenge', r'\blacksquare'],
     [r'\ldots', r'\iint', r'\mho', r'\blacktriangle'],
@@ -166,7 +168,6 @@ MISCELLANEUOS_SYMBOLS = [
     [r'\Im', r'\flat', r'\square', r'\backprime'],
     [r'\Re', r'\natural', r'\surd', r'\circledS'],
 ]
-
 MATH_MODE_ACCENTS = [
     [r'\acute{a}', r'\bar{a}', r'\Acute{\Acute{A}}', r'\Bar{\Bar{A}}'],
     [r'\breve{a}', r'\check{a}', r'\Breve{\Breve{A}}', r'\Check{\Check{A}}'],
@@ -174,12 +175,9 @@ MATH_MODE_ACCENTS = [
     [r'\grave{a}', r'\hat{a}', r'\Grave{\Grave{A}}', r'\Hat{\Hat{A}}'],
     [r'\tilde{a}', r'\vec{a}', r'\Tilde{\Tilde{A}}', r'\Vec{\Vec{A}}'],
 ]
-
 OTHER_STYLES_MATH_MODE_ONLY = [
     [r'\mathcal{ABCDEFGHIJKLMNOPQRSTUVWXYZ}'],
-    [r'\mathcal{abcdefghijklmnopqrstuvwxyz}'],
     [r'\mathbb{ABCDEFGHIJKLMNOPQRSTUVWXYZ}'],
-    [r'\mathbb{abcdefghijklmnopqrstuvwxyz}'],
     [r'\mathfrak{ABCDEFGHIJKLMNOPQRSTUVWXYZabc123}'],
     [r'\mathsf{ABCDEFGHIJKLMNOPQRSTUVWXYZabc123}'],
     [r'\mathbf{ABCDEFGHIJKLMNOPQRSTUVWXYZabc123}'],
@@ -218,7 +216,7 @@ class TestAll(BaseScene):
                 G.append(VGroup(*H).arrange(RIGHT))
             G = VGroup(*G).arrange(DOWN)
             self.add(G)
-            self.wait(10)
+            self.wait(5)
             self.remove(G)
 
         def transpose(tokens: List[List[str]]):
@@ -227,10 +225,15 @@ class TestAll(BaseScene):
                 H = []
                 for col in range(len(tokens)):
                     token = tokens[col][row]
-                    tex = self.make_tex(token)
-                    text = self.make_text(token)
-                    H.append(tex)
-                    H.append(text)
+
+                    #tex = self.make_tex(token)
+                    #text = self.make_text(token)
+                    #H.append(tex)
+                    #H.append(text)
+
+                    textext = self.make_tex(f'{token}&\\displaytext{{ {token} }}')
+                    H.append(textext)
+
                 G.append(VGroup(*H).arrange(RIGHT))
             G = VGroup(*G).arrange(DOWN)
             self.add(G)
@@ -242,14 +245,18 @@ class TestAll(BaseScene):
         interpose(DELIMITERS)
         interpose(VARIABLE_SIZED_SYMBOLS)
         interpose(STANDARD_FUNCTION_NAMES)
-        interpose(BINARY_OPERATION_RELATION_SYMBOLS_1)
+        interpose(BINARY_OPERATION_RELATION_SYMBOLS_1_A)
+        interpose(BINARY_OPERATION_RELATION_SYMBOLS_1_B)
         interpose(BINARY_OPERATION_RELATION_SYMBOLS_2)
-        interpose(BINARY_OPERATION_RELATION_SYMBOLS_3)
-        interpose(BINARY_OPERATION_RELATION_SYMBOLS_4)
-        interpose(ARROW_SYMBOLS1)
-        interpose(ARROW_SYMBOLS2)
-        interpose(ARROW_SYMBOLS3)
-        interpose(ARROW_SYMBOLS4)
-        interpose(MISCELLANEUOS_SYMBOLS)
+        interpose(BINARY_OPERATION_RELATION_SYMBOLS_3_A)
+        interpose(BINARY_OPERATION_RELATION_SYMBOLS_3_B)
+        interpose(BINARY_OPERATION_RELATION_SYMBOLS_4_A)
+        interpose(BINARY_OPERATION_RELATION_SYMBOLS_4_B)
+        interpose(ARROW_SYMBOLS_1)
+        interpose(ARROW_SYMBOLS_2)
+        interpose(ARROW_SYMBOLS_3)
+        interpose(ARROW_SYMBOLS_4)
+        interpose(MISCELLANEUOS_SYMBOLS_1)
+        interpose(MISCELLANEUOS_SYMBOLS_2)
         interpose(MATH_MODE_ACCENTS)
-        #interpose(OTHER_STYLES_MATH_MODE_ONLY)
+        interpose(OTHER_STYLES_MATH_MODE_ONLY)
