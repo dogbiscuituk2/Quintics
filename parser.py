42740bb13c978bece4b34009c3ca7cbe268c8483
John Kerr <john@jmkerr.com>
2024-12-15 14:19:38 +0000

    fiddler on the roof

--- parser.py
+++ parser.py
@@ -1,9 +1,24 @@
-from context import Context
 from manim import *
 import re
 
 PAT_TOKEN = r"\\{|\\}|\\\||\\[A-Za-z]+|\\\\|[^&\s]"
 
+class Term():
+
+    token_index: int
+    token_count: int
+    glyph_index: int
+    glyph_count: int
+
+    def __init__(
+            self,
+            token_index: int,
+            glyph_index: int,
+            glyph_count: int = 1):
+        token_index = token_index
+        glyph_index = glyph_index
+        glyph_count = glyph_count
+        
 class Parser():
 
     _glyph_index: int
@@ -16,9 +31,10 @@ class Parser():
         self._token_index = 0
         self._tex = tex
         self._tokens = re.findall(PAT_TOKEN, tex.tex_string)
+        print(self._tokens)
 
     def parse(self) -> None:
-        self._parse_string(Context())
+        self._parse_string()
 
     @property
     def _more(self) -> bool:
@@ -34,54 +50,47 @@ class Parser():
     def _peek(self) -> str:
         return self._tokens[self._token_index]
 
-    @property
-    def _size(self) -> int:
-        try:
-            tex = MathTex(self._peek)
-        except ValueError:
-            return -1
-        return len(tex[0])
-
     def _accept(self, token: str) -> None:
         if (self._peek != token):
             pass
         self._token_index += 1
 
-    def _parse_expression(self, context: Context) -> None:
-        self._accept('{')
-        context.level += 1
-        self._parse_string(context)
-        self._accept('}')
-        context.level -= 1
+    def _parse_shift(self, token: str) -> List[Term]:
+        self._accept(token)
+        return self._parse_atom()
     
-    def _parse_string(self, context: Context) -> None:
+    def _parse_string(self) -> None:
         while self._more and self._peek != '}':
-            self._parse_token(context)
+            self._parse_atom()
 
-    def _parse_subscript(self, context) -> None:
-        self._accept('_')
-        context.sub += 1
-        self._parse_token(context)
-        context.sub -= 1
+    def _parse_subscript(self) -> List[Term]:
+        return self._parse_shift('_')
 
-    def _parse_superscript(self, context) -> None:
-        self._accept('^')
-        context.super += 1
-        self._parse_token(context)
-        context.super -= 1
+    def _parse_superscript(self) -> List[Term]:
+        return self._parse_shift('^')
 
-    def _parse_token(self, context: Context) -> None:
+    def _parse_sum(self) -> List[Term]:
+        middle = Term(self._token_index, self._glyph_index)
+        self._token_index += 1
+        bottom = self._parse_subscript() if self._peek == '_' else None
+        top = self._parse_superscript() if self._peek == '^' else None
+
+    def _parse_atom(self) -> List[Term]:
         match self._peek:
+            case r'\sum':
+                return self._parse_sum()
             case '{':
-                self._parse_expression(context)
-            case '_':
-                self._parse_subscript(context)
-            case '^':
-                self._parse_superscript(context)
+                self._accept('{')
+                term = self._parse_atom()
+                self._accept('}')
+                return term
             case _:
                 token = self._next
-                print(token, context)
+                print(token)
 
 if (__name__ == '__main__'):
-    parser = Parser(MathTex(r'\frac{x_1}{y^2}\sum_{n=0}^\infty{x^n}'))
+
+    parser = Parser(MathTex(r'\sum_{i=0}^{n+1}{i^2}'))
+    print('ok1')
     parser.parse()
+    print('ok2')
