2dd39011660d71630420f81642ff46c8972bfece
John Kerr <john@jmkerr.com>
2024-12-16 23:26:53 +0000

    sort

--- parser.py
+++ parser.py
@@ -68,23 +68,27 @@ class Parser():
             return 0
         return len(tex[0])
 
-    def _parse_shift(self, token: str) -> List[Term]:
-        '''
-        If the current token is either '_' or '^' then return the following atom,
-        otherwise return the empty list.
-
-        str: The expected token, '_' or '^'.
-        '''
-        if self._peek == token:
-            self._accept(token)
-            return self._parse_atom()
-        return []
-    
-    def _parse_string(self) -> List[Term]:
-        result = []
-        while self._more and self._peek != '}':
-            result += self._parse_atom()
-        return result
+    def _parse_atom(self) -> List[Term]:
+        token = self._peek
+        if re.match(PAT_LARGE, token):
+            return self._parse_large()
+        match token:
+            case '{':
+                self._accept('{')
+                result = self._parse_string()
+                self._accept('}')
+                return result
+            case '_':
+                return self._parse_shift(token)
+            case '^':
+                return self._parse_shift(token)
+            case _:
+                glyph_count = self._get_tex_len(token)
+                result = [Term(self._token_index, self._glyph_index, glyph_count)]
+                token = self._next
+                print(token)
+                self._glyph_index += glyph_count
+                return result
 
     def _parse_large(self) -> List[Term]:
         middle = [Term(self._token_index, self._glyph_index)]
@@ -113,27 +117,23 @@ class Parser():
 
         return top + middle + bottom
 
-    def _parse_atom(self) -> List[Term]:
-        token = self._peek
-        if re.match(PAT_LARGE, token):
-            return self._parse_large()
-        match token:
-            case '{':
-                self._accept('{')
-                result = self._parse_string()
-                self._accept('}')
-                return result
-            case '_':
-                return self._parse_shift(token)
-            case '^':
-                return self._parse_shift(token)
-            case _:
-                glyph_count = self._get_tex_len(token)
-                result = [Term(self._token_index, self._glyph_index, glyph_count)]
-                token = self._next
-                print(token)
-                self._glyph_index += glyph_count
-                return result
+    def _parse_shift(self, token: str) -> List[Term]:
+        '''
+        If the current token is either '_' or '^' then return the following atom,
+        otherwise return the empty list.
+
+        str: The expected token, '_' or '^'.
+        '''
+        if self._peek == token:
+            self._accept(token)
+            return self._parse_atom()
+        return []
+    
+    def _parse_string(self) -> List[Term]:
+        result = []
+        while self._more and self._peek != '}':
+            result += self._parse_atom()
+        return result
 
 if (__name__ == '__main__'):
 
