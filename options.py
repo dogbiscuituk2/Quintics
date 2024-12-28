78689862a07e9b873a02897076c84a9aebd02f5f
John Kerr <john@jmkerr.com>
2024-12-28 19:25:42 +0000

    more options

--- options.py
+++ options.py
@@ -11,23 +11,31 @@ class Opt(Flag):
     SUB     = auto() # 'x_1': '1' matches 'x'.
     SUPER   = auto() # 'x^2': '2' matches 'x'.
     DIFF    = auto() # r'\frac{d^2x}{dy^2}': 'd' & '2' match 'x' or 'y'.
+    MATH    = auto() # r'\overbrace{x}': brace matches'x'.
     ACCENT  = auto() # r'acute{e}': the accent matches the letter.
     SHIFT   = SUB | SUPER
-    ALL     = SHIFT | DIFF | ACCENT
+    SYM     = MATH | ACCENT
+    ALL     = SHIFT | DIFF | SYM
 
 print(
     Opt.NONE.value,
     Opt.SUB.value,
     Opt.SUPER.value,
-    Opt.SHIFT.value,
     Opt.DIFF.value,
+    Opt.MATH.value,
+    Opt.ACCENT.value,
+    Opt.SHIFT.value,
+    Opt.SYM.value,
     Opt.ALL.value,
 )
-opts = Opt.SUB | Opt.SUPER | Opt.DIFF
+opts = Opt.SHIFT | Opt.DIFF | Opt.ACCENT
 
 print(f"{Opt.NONE in opts = }")
 print(f"{Opt.SUB in opts = }")
 print(f"{Opt.SUPER in opts = }")
-print(f"{Opt.SHIFT in opts = }")
 print(f"{Opt.DIFF in opts = }")
+print(f"{Opt.MATH in opts = }")
+print(f"{Opt.ACCENT in opts = }")
+print(f"{Opt.SHIFT in opts = }")
+print(f"{Opt.SYM in opts = }")
 print(f"{Opt.ALL in opts = }")
