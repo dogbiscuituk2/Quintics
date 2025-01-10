3cef8830eaecb678d088f8f9534a2aa573d4f1cf
John Kerr <john@jmkerr.com>
2025-01-10 15:18:50 +0000

    back2work

---
+++ labels.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from manim import *
+from typing import Type
+
+#def get_labels(*objs: Mobject) -> VGroup:
+#    labels = VGroup()
+#
+#    def add(parent: Mobject) -> None:
+#        if isinstance(parent, SVGMobject):
+#            labels.add(index_labels(parent))
+#        if isinstance(parent, VMobject):
+#            for child in parent.submobjects:
+#                add(child)
+#
+#    for obj in objs:
+#        add(obj)
+#    return labels
+
+def get_labels(*objs: Mobject) -> VGroup:
+    labels = VGroup()
+
+    def add(parent: Mobject) -> None:
+        if isinstance(parent, SVGMobject):
+            labels.add(index_labels(parent))
+        if isinstance(parent, VMobject):
+            for child in parent.submobjects:
+                add(child)
+
+    for obj in objs:
+        add(obj)
+    return labels
