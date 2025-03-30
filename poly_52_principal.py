0d254b459f93759381894e44584f5c57dfb91307
John Kerr <john@jmkerr.com>
2025-03-30 17:35:18 +0100

    xfer

---
+++ poly_52_principal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+from base_scene import Animate, BaseScene
+from labels import *
+import math
+from MF_Tools import *
+from painter import *
+
+e1 = (
+    'z &= x²+hx+k',
+    'y &= x⁵+px³+qx²+rx+s=0',
+    'y &= z⁵+0z³+uz²+vz+w=0',
+    ''
+)
--- poly_91_newton.py
+++ poly_91_newton.py
@@ -6,7 +6,23 @@
 
 from base_scene import Animate, BaseScene
 from painter import *
-from equations.poly_91 import *
+
+#region data
+
+e1 = (
+    r'y &= \sum_{i=0}^{n>0} a_i x^i, \quad a_n=1',
+    r'&= x^n + a_{n-1}x^{n-1} + a_{n-2}x^{n-2} + \ldots + a_2 x^2 + a_1 x + a_0',
+    r'&= (x-x_1)(x-x_2) \ldots (x-x_n)',
+    r'&= \prod_{j=1}^{n} (x-x_j)',
+#),(
+#    r'S_{m>0} &= \sum_{j=1}^{n} x_j^m = x_1^m + x_2^m + \ldots + x_n^m',
+#),(
+#    r'S_m &= -ma_{n-m}-\sum_{j=1}^{m-1} a_{j+n-m} S_j, \quad a_{j<0}=0',
+)
+
+#endregion
+
+#region code
 
 class Poly_91_Newton(BaseScene):
 
@@ -24,30 +40,36 @@ class Poly_91_Newton(BaseScene):
             ('y', Pen.MAGENTA),
             ('z', Pen.CYAN)))
 
-        E = self.make_texes(e1, e2, e4, e5)
+        E1 = self.make_texes(*e1)
 
-        with self.say("Let y be a polynomial, of degree n greater than zero, with coefficients a i,"):
-            self.play(Create(E[0]))
+        with self.say("Let y be a monic polynomial of degree n greater than zero, with coefficients a i. "):
+            self.play(Create(E1[0]))
+            self.play(Create(E1[1]))
 
-        with self.say("and roots denoted by x j, for j between 1 and n,"):
-            self.play(Create(E[1]))
+        with self.say("It has n roots, denoted by x 1, x 2, and so on, up to x n, "):
+            self.play(Create(E1[2]))
 
-        with self.say("and let S m be the sum of the m-th powers of these roots."):
-            self.play(Create(E[2]))
+        with self.say("so it can also be expressed as the product of n linear factors. "):
+            self.play(Create(E1[3]))
 
-        with self.say("We need to prove this recurrence relation between the power sums,"):
-            p = 27
-            self.box_on(E[3][0][:p])
-            self.play(Create(E[3][0][:p]))
+        #with self.say("and let S m be the sum of the m-th powers of these roots."):
+        #    self.play(Create(E1[2]))
 
-        with self.say("where all coefficients with a negative index are taken to be zero."):
-            self.box_on(E[3][0][p+1:])
-            self.play(FadeIn(E[3][0][p:]))
+        #with self.say("We need to prove this recurrence relation between the power sums,"):
+        #    p = 25
+        #    self.box_on(E1[3][0][:p])
+        #    self.play(Create(E1[3][0][:p]))
 
-        self.box_off()
+        #with self.say("where all coefficients with a negative index are taken to be zero."):
+        #    self.box_on(E1[3][0][p+1:])
+        #    self.play(FadeIn(E1[3][0][p:]))
+
+        #self.box_off()
 
         self.wait(10)
         return
+
+#endregion
 
 if __name__ == "__main__":
     BaseScene.run(__file__)
