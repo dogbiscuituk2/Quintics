566bb55c6160ece7a4b9d43298e2a59651ecfaa7
John Kerr <john@jmkerr.com>
2025-04-20 11:14:10 +0100

    xfer

--- poly_52_powersums.py
+++ poly_52_powersums.py
@@ -34,29 +34,29 @@ e3 = (
 
 e4 = (
     r'S_m &= -ma_{5-m} - \sum_{j=1}^{m-1} a_{5-m+j} S_j, \quad a_{j<0} = 0',
-     'S_1 &= -a_4',
-     'S_2 &= -2a_3 - a_4 S_1',
-     'S_3 &= -3a_2 - a_3 S_1 - a_4 S_2',
-     'S_4 &= -4a_1 - a_2 S_1 - a_3 S_2 - a_4 S_3',
-     'S_5 &= -5a_0 - a_1 S_1 - a_2 S_2 - a_3 S_3 - a_4 S_4',
-     'S_6 &= -a_0 S_1 - a_1 S_2 - a_2 S_3 - a_3 S_4 - a_4 S_5',
-     'S_7 &= -a_0 S_2 - a_1 S_3 - a_2 S_4 - a_3 S_5 - a_4 S_6',
-     'S_8 &= -a_0 S_3 - a_1 S_4 - a_2 S_5 - a_3 S_6 - a_4 S_7',
-     'S_9 &= -a_0 S_4 - a_1 S_5 - a_2 S_6 - a_3 S_7 - a_4 S_8',
-    'S_10 &= -a_0 S_5 - a_1 S_6 - a_2 S_7 - a_3 S_8 - a_4 S_9',
+    'S_1 &= -a_4',
+    'S_2 &= -2a_3 - a_4 S_1',
+    'S_3 &= -3a_2 - a_3 S_1 - a_4 S_2',
+    'S_4 &= -4a_1 - a_2 S_1 - a_3 S_2 - a_4 S_3',
+    'S_5 &= -5a_0 - a_1 S_1 - a_2 S_2 - a_3 S_3 - a_4 S_4',
+    'S_6 &= -a_0 S_1 - a_1 S_2 - a_2 S_3 - a_3 S_4 - a_4 S_5',
+    'S_7 &= -a_0 S_2 - a_1 S_3 - a_2 S_4 - a_3 S_5 - a_4 S_6',
+    'S_8 &= -a_0 S_3 - a_1 S_4 - a_2 S_5 - a_3 S_6 - a_4 S_7',
+    'S_9 &= -a_0 S_4 - a_1 S_5 - a_2 S_6 - a_3 S_7 - a_4 S_8',
+    'S_{10} &= -a_0 S_5 - a_1 S_6 - a_2 S_7 - a_3 S_8 - a_4 S_9',
 )
 
-e5 = (
-     'S_1 &= -a',
-     'S_2 &= -2b - a S_1',
-     'S_3 &= -3c - b S_1 - a S_2',
-     'S_4 &= -4d - c S_1 - b S_2 - a S_3',
-     'S_5 &= -5e - d S_1 - c S_2 - b S_3 - a S_4',
-     'S_6 &= -e S_1 - d S_2 - c S_3 - b S_4 - a S_5',
-     'S_7 &= -e S_2 - d S_3 - c S_4 - b S_5 - a S_6',
-     'S_8 &= -e S_3 - d S_4 - c S_5 - b S_6 - a S_7',
-     'S_9 &= -e S_4 - d S_5 - c S_6 - b S_7 - a S_8',
-    'S_10 &= -e S_5 - d S_6 - c S_7 - b S_8 - a S_9',
+e6 = (
+    'S_1 &= -a',
+    'S_2 &= -2b - a S_1',
+    'S_3 &= -3c - b S_1 - a S_2',
+    'S_4 &= -4d - c S_1 - b S_2 - a S_3',
+    'S_5 &= -5e - d S_1 - c S_2 - b S_3 - a S_4',
+    'S_6 &= -e S_1 - d S_2 - c S_3 - b S_4 - a S_5',
+    'S_7 &= -e S_2 - d S_3 - c S_4 - b S_5 - a S_6',
+    'S_8 &= -e S_3 - d S_4 - c S_5 - b S_6 - a S_7',
+    'S_9 &= -e S_4 - d S_5 - c S_6 - b S_7 - a S_8',
+    'S_{10} &= -e S_5 - d S_6 - c S_7 - b S_8 - a S_9',
 )
 
 #endregion
@@ -81,12 +81,14 @@ class Poly_52_PowerSums(BaseScene):
         E1 = self.make_texes(*e1)
         E2 = self.make_texes(e2[0], e1[4], *e3[:3])
         E3 = self.make_texes(*e3[1:])
-        E4 = self.make_texes(*e4)
+        E4 = self.make_texes(*e4[:9])
+        E5 = self.make_texes(*e4[1:])
+        E6 = self.make_texes(*e6)
         
         with self.say("This is the general polynomial of degree n, in one variable, x."):
             self.play(Create(E1[0]))
 
-        with self.say("Let's unpack this sum."):
+        with self.say("Let's unpack it."):
             arc = {"path_arc": -PI/2}
             self.play(
                 TransformByGlyphMap(
@@ -100,7 +102,6 @@ class Poly_52_PowerSums(BaseScene):
                     (GrowFromCenter, [6,15,*ir(24,28),32]),
                 ),
                 run_time=2)
-            
             
         with self.say("We can always scale things so this leading term has a coefficient of one."):
             E1[1] = self.autopilot(E1[1], e2[0], 2, 6, 4, ([2,3], FadeOut))
@@ -172,14 +173,28 @@ class Poly_52_PowerSums(BaseScene):
                 ReplacementTransform(E2[4], E3[1]))
             self.play(Create(E3[2:6]))
 
-        with self.say("Since we are dealing with the quintic, let's now replace n with 5."):
-            #self.wait()
+        with self.say("Since we are dealing with the quintic, let's now replace n with 5, "):
             self.play(Create(E3[6:]))
-            #self.wait()
-            for row in range(6):
-                self.play(ReplacementTransform(E3[row], E4[row]))
-            #self.wait()
+            self.play([ReplacementTransform(E3[row], E4[row]) for row in range(6)])
             self.play(Create(E4[6:9]))
+
+        def foo(row: int, map) -> None:
+            return TransformByGlyphMap(E5[row], E6[row], (map, FadeOut))
+
+        def bar(row: int, map) -> None:
+            self.play(foo(row, map))
+
+        with self.say("and the coefficients with the original single letter ones, a b c d e."):
+            self.play(FadeOut(E3[0]))
+            self.play(TransformMatchingShapes(E4, E5))
+            self.wait(5)
+            bar(0, ([5]))
+            bar(1, ([6,9]))
+            bar(2, ([6,9,14]))
+            bar(3, ([6,9,14,19]))
+            bar(4, ([6,9,14,19,24]))
+            self.play(*[foo(row, ([5,10,15,20,25])) for row in range(5,9)])
+            bar(9, ([6,11,16,21,26]))
 
         self.wait(10)
 
